e6f050cf7cf33db97c4f39958cea9211d202a0a5
karimulla begam <chkarimullabegam7893@gmail.com>
2025-03-12 10:01:36 +0530

    first commit

---
+++ Task1.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentGrades {
+    
+    public static double calculateAverage(ArrayList<Integer> grades) {
+        int total = 0;
+        for (int grade : grades) {
+            total += grade;
+        }
+        return total / (double) grades.size(); 
+    }
+    
+  
+    public static int calculateHighest(ArrayList<Integer> grades) {
+        int highest = grades.get(0);
+        for (int grade : grades) {
+            if (grade > highest) {
+                highest = grade;
+            }
+        }
+        return highest;
+    }
+    
+    public static int calculateLowest(ArrayList<Integer> grades) {
+        int lowest = grades.get(0);
+        for (int grade : grades) {
+            if (grade < lowest) {
+                lowest = grade;
+            }
+        }
+        return lowest;
+    }
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> grades = new ArrayList<>();
+        int numberOfStudents;
+        
+    
+        System.out.print("Enter the number of students: ");
+        numberOfStudents = scanner.nextInt();
+        
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.print("Enter grade for student " + (i + 1) + ": ");
+            int grade = scanner.nextInt();
+            grades.add(grade); 
+        }
+        
+     
+        double average = calculateAverage(grades);
+        int highest = calculateHighest(grades);
+        int lowest = calculateLowest(grades);
+    
+        System.out.println("\nResults:");
+        System.out.println("Average Grade: " + average);
+        System.out.println("Highest Grade: " + highest);
+        System.out.println("Lowest Grade: " + lowest);
+        
+      
+        scanner.close();
+    }
+    
+}
